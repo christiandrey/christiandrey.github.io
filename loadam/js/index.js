42ce18144584cfb59496002cfae6aba98e591555
Christian Dréy <Christian Dry>
2016-09-10 17:42:48 +0100

    Finished Skeletal HomePage; Not yet Desktop Friendly; No Animations Yet

---
+++ loadam/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.navCloseBtn, .navToggle').click(function(){
+        $('.mainPage').toggleClass('scaleClass');
+        $('.nextPage').toggleClass('scalenextPage');
+        $('.lastPage').toggleClass('scalelastPage');
+    })
+})
