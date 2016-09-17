a135194554e5df2e0d628855854920981ee1c468
Christian Dréy <Christian Dry>
2016-09-17 07:02:08 +0100

    Added Minimal Animations, Fixed Desktop Mode, Stiill needs a bit of work though.

--- loadam/js/index.js
+++ loadam/js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+    $('.heroAnimationBus').addClass('moveBus');
     $('.navCloseBtn, .navToggle').click(function () {
+        $('.navItems').find('a').toggleClass('navItemAnimation');
         $('.mainPage').toggleClass('scaleClass');
         $('.nextPage').toggleClass('scalenextPage');
         $('.lastPage').toggleClass('scalelastPage');
