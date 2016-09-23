94742ba8b76b49a3d8712c9e0655743b7e9fd754
Christian Dréy <Christian Dry>
2016-09-23 02:58:42 +0100

    Finished Page Designs. Relinking begun

--- stclairs/js/index.js
+++ stclairs/js/index.js
@@ -1,10 +1,3 @@
 $(document).ready(function () {
     $('.pageSlider').unslider({autoplay: true, arrows: false});
-    $('.heroAnimationBus').addClass('moveBus');
-    $('.navCloseBtn, .navToggle').click(function () {
-        $('.navItems').find('a').toggleClass('navItemAnimation');
-        $('.mainPage').toggleClass('scaleClass');
-        $('.nextPage').toggleClass('scalenextPage');
-        $('.lastPage').toggleClass('scalelastPage');
-    })
 })
