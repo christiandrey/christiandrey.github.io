bdf534cfe4903682a60b4eaae3de451ed72136c8
christiandrey <oluwaseun.adedire@gmail.com>
2017-01-16 05:42:30 +0100

    Finished Login Page

--- acga9/js/index.js
+++ acga9/js/index.js
@@ -17,15 +17,14 @@ $(document).ready(function(){
       animationDelay : 550
     })
 
-    $('.loginTypeIndicator').addClass("bgAccent").dreyanim({
+    $('.loginTypeIndicator').addClass("bgAccentTrans").dreyanim({
       animationType : "slideInFromDown",
       animationTime : 450,
-      animationDelay : 550
+      animationDelay : 0
     });
 
     $('.landingWrapper').addClass("landingWrapperModded");
   })
-
   $('.userLogin').click(function(){
     $('.adminLogin').dreyanim({
       animationType : "slideOutToUp",
@@ -44,12 +43,36 @@ $(document).ready(function(){
       animationDelay : 550
     })
 
-    $('.loginTypeIndicator').addClass("bgPrimary").dreyanim({
+    $('.loginTypeIndicator').addClass("bgPrimaryTrans").dreyanim({
       animationType : "slideInFromDown",
       animationTime : 450,
-      animationDelay : 550
+      animationDelay : 0
     });
 
     $('.landingWrapper').addClass("landingWrapperModded");
+  })
+  $('.backLink').click(function(){
+    $formID = $(this).parent().attr("data-formID");
+    eval("$formType = $('."+$formID+"FormWrapper')");
+    $formType.dreyanim({
+      animationType : "slideOutToUp",
+      animationTime : 450
+    })
+    $('.adminLogin').dreyanim({
+      animationType : "slideInFromDown",
+      animationTime : 450,
+      animationDelay : 450
+    })
+    $('.userLogin').dreyanim({
+      animationType : "slideInFromDown",
+      animationTime : 450,
+      animationDelay : 550
+    })
+    $('.loginTypeIndicator').removeClass("bgPrimaryTrans, bgAccentTrans").dreyanim({
+      animationType : "slideOutToUp",
+      animationTime : 450,
+      animationDelay : 550
+    })
+    $('.landingWrapper').removeClass("landingWrapperModded");
   })
 })
