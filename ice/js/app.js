83fe867e1f62a40eca5f8b91920097797d0ea938
Adedire Oluwaseun <oluwaseunadedire@live.com>
2017-05-06 19:58:10 +0100

    Switched to swivel nav Switched to swivel nav

--- ice/js/app.js
+++ ice/js/app.js
@@ -1,25 +1,56 @@
 $(document).ready(function(e) {
-	var isNavOpen = true;
-	$('.nav-trigger>img').click(function(e) {
-		if(isNavOpen) {
-			$(this).attr('src','img/icon.hamburger-close.png');
-			$('body').css('overflow', 'hidden');
-			$('.nav-drawer .nav-item').dreyanim({
-				animationType: 'slideInFromDown',
-				animationTime: 450
-			})
+	// var isNavOpen = true;
+	// $('.nav-trigger>img').click(function(e) {
+	// 	if(isNavOpen) {
+	// 		$(this).attr('src','img/icon.hamburger-close.png');
+	// 		$('body').css('overflow', 'hidden');
+	// 		$('.nav-drawer .nav-item').dreyanim({
+	// 			animationType: 'slideInFromDown',
+	// 			animationTime: 450
+	// 		})
 
-			isNavOpen = false;
-		} else {
-			$(this).attr('src','img/icon.hamburger.png');
-			$('body').css('overflow', 'auto');
-			$('.nav-drawer .nav-item').addClass('hidden');
-			isNavOpen = true;
-		}
-		$(this).parent().toggleClass('open');
+	// 		isNavOpen = false;
+	// 	} else {
+	// 		$(this).attr('src','img/icon.hamburger.png');
+	// 		$('body').css('overflow', 'auto');
+	// 		$('.nav-drawer .nav-item').addClass('hidden');
+	// 		isNavOpen = true;
+	// 	}
+	// 	$(this).parent().toggleClass('open');
 
-		$('.nav-drawer').toggleClass('hidden');
+	// 	$('.nav-drawer').toggleClass('hidden');
 
-		$('.section-indicator').toggleClass('hidden');
-	})
+	// 	$('.section-indicator').toggleClass('hidden');
+	// })
+
+	 //whoweare
+	  $.easing.custom = function (x, t, b, c, d) {
+	    var s = 1.70158;
+	    if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+	    return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+	  }
+
+	  $('.navContainer').click(function(){
+	    $('.ham').toggleClass("open");
+	    $('.navElements').toggleClass("close");
+	    $('.navElements').toggleClass("open");
+	    $('.navElements *').toggleClass("open");
+	  })
+
+	  $('.navElements .home').click(function(){
+	    location.href="index.html"
+	  })
+
+	  $('.navElements .whoweare').click(function(){
+	    location.href="whoweare.html"
+	  })
+
+	  $('.navElements .whatwedo').click(function(){
+	    location.href="whatwedo.html"
+	  })
+
+	  $('.navElements .contact').click(function(){
+	    location.href="contact.html"
+	  })
+	  //--End of Navigation
 })
