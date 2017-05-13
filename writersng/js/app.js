0759957cf8ddf94ec4c6c636178a659d0be778b1
Adedire Oluwaseun <oluwaseunadedire@live.com>
2017-05-13 04:25:55 +0100

    Finished Homepage

--- writersng/js/app.js
+++ writersng/js/app.js
@@ -1,3 +1,46 @@
 $(document).ready(function(e) {
 	$('.col-match').matchHeight();
+
+	var sliderVariable = new Number;
+	sliderVariable = 0;
+	$('#index-page .rarr').on("click", function(e) {
+		sliderVariable -= 100;
+		switch(sliderVariable<-100) {
+			case false:
+			$('#index-page .l-section.testimony .col-content-stage').css('left',sliderVariable+"%");
+			break;
+			case true:
+			sliderVariable = 0;
+			$('#index-page .l-section.testimony .col-content-stage').css('left',sliderVariable+"%");			
+			break;
+		}
+	})
+
+	$('#index-page .larr').on("click", function(e) {
+		sliderVariable += 100;
+		switch(sliderVariable>0) {
+			case false:
+			$('#index-page .l-section.testimony .col-content-stage').css('left',sliderVariable+"%");
+			break;
+			case true:
+			sliderVariable = 0;			
+			break;
+		}
+	})
+
+	$('#index-page .larr').mouseenter(function(e) {
+		$(this).attr('src','img/icon.left--hover.png')
+	})
+
+	$('#index-page .larr').mouseleave(function(e) {
+		$(this).attr('src','img/icon.left.png')
+	})
+
+	$('#index-page .rarr').mouseenter(function(e) {
+		$(this).attr('src','img/icon.right--hover.png')
+	})
+
+	$('#index-page .rarr').mouseleave(function(e) {
+		$(this).attr('src','img/icon.right.png')
+	})
 })
