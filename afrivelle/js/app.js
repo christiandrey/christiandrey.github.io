884329985ca2f973ab28f7f6dbda961a6fe853cf
Adedire Oluwaseun <oluwaseun.adedire@gmail.com>
2017-06-29 07:50:18 +0100

    Left with Owl carousel

--- afrivelle/js/app.js
+++ afrivelle/js/app.js
@@ -14,4 +14,53 @@ $(document).ready(function(e) {
 			$(this).siblings(".form-label").removeClass("is-focused");
 		}
 	})
+
+	var waypoint = new Waypoint({
+		element: document.getElementById('home'),
+		handler: function() {
+			$('.l-nav .indicator').css("left",0);
+			$('.l-nav .nav-main .brand-icon').css("margin-left","-84px")
+		}
+	})
+
+	var waypoint = new Waypoint({
+		element: document.getElementById('who-we-are'),
+		handler: function() {
+			$('.l-nav .indicator').css("left","25%");
+			$('.l-nav .nav-main .brand-icon').css("margin-left","0")
+		},
+		offset: 80
+	})
+
+	var waypoint = new Waypoint({
+		element: document.getElementById('what-we-do'),
+		handler: function() {
+			$('.l-nav .indicator').css("left","50%")
+		},
+		offset: 80
+	})
+
+	var waypoint = new Waypoint({
+		element: document.getElementById('contact-us'),
+		handler: function() {
+			$('.l-nav .indicator').css("left","75%")
+		},
+		offset: 100
+	})
+
+	$(".nav-link").click(function(e) {
+		var scroll = "#" + $(this).attr("data-href");
+		$.scrollTo(scroll, {
+			duration: 600,
+			offset: -80
+		})
+	})
+
+	$("a.get-in-touch").click(function(e) {
+		e.preventDefault();
+		$.scrollTo("#contact-us", {
+			duration: 600,
+			offset: -80
+		})
+	})
 })
