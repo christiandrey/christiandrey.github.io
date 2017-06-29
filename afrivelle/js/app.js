b0e4f08e9008b08e675c6bfd712ee9acbd3c67a5
Adedire Oluwaseun <oluwaseun.adedire@gmail.com>
2017-06-29 03:22:46 +0100

    Almost done

--- afrivelle/js/app.js
+++ afrivelle/js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function(e) {
-	//Start writing some awesome code
+	// $(".form-item input, .form-item textarea").on(focus, function(e) {
+		
+	// })
+
+	$(".form-item input, .form-item textarea").focusin(function(e) {
+		if ($(this).val() == "" || $(this).val() == null) {
+			$(this).siblings(".form-label").addClass("is-focused");
+		}
+	})
+
+	$(".form-item input, .form-item textarea").focusout(function(e) {
+		if ($(this).val() == "" || $(this).val() == null) {
+			$(this).siblings(".form-label").removeClass("is-focused");
+		}
+	})
 })
