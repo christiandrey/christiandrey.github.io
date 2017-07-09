6f480b656332d424f524d1523825c8d61702ea77
Adedire Oluwaseun <oluwaseun.adedire@gmail.com>
2017-07-09 12:07:13 +0100

    Afrivelle Correction

--- afrivelle/js/app.js
+++ afrivelle/js/app.js
@@ -1,76 +1,85 @@
 $(document).ready(function(e) {
 
-	$(".form-item input, .form-item textarea").focusin(function(e) {
-		if ($(this).val() == "" || $(this).val() == null) {
-			$(this).siblings(".form-label").addClass("is-focused");
-		}
-	})
+    $(".form-item input, .form-item textarea").focusin(function(e) {
+        if ($(this).val() == "" || $(this).val() == null) {
+            $(this).siblings(".form-label").addClass("is-focused");
+        }
+    });
 
-	$(".form-item input, .form-item textarea").focusout(function(e) {
-		if ($(this).val() == "" || $(this).val() == null) {
-			$(this).siblings(".form-label").removeClass("is-focused");
-		}
-	})
+    $(".form-item input, .form-item textarea").focusout(function(e) {
+        if ($(this).val() == "" || $(this).val() == null) {
+            $(this).siblings(".form-label").removeClass("is-focused");
+        }
+    });
 
-	var waypoint = new Waypoint({
-		element: document.getElementById('home'),
-		handler: function() {
-			$('.l-nav .indicator').css("left",0);
-			$('.l-nav .nav-main .brand-icon').css("margin-left","-84px")
-		}
-	})
+    var waypoint = new Waypoint({
+        element: document.getElementById('home'),
+        handler: function() {
+            $('.l-nav .indicator').css("left", 0);
+            $('.l-nav .nav-main .brand-icon').css("margin-left", "-84px")
+        }
+    });
 
-	var waypoint = new Waypoint({
-		element: document.getElementById('who-we-are'),
-		handler: function() {
-			$('.l-nav .indicator').css("left","25%");
-			$('.l-nav .nav-main .brand-icon').css("margin-left","0")
-		},
-		offset: 80
-	})
+    var waypoint = new Waypoint({
+        element: document.getElementById('who-we-are'),
+        handler: function() {
+            $('.l-nav .indicator').css("left", "25%");
+            $('.l-nav .nav-main .brand-icon').css("margin-left", "0")
+        },
+        offset: 80
+    });
 
-	var waypoint = new Waypoint({
-		element: document.getElementById('what-we-do'),
-		handler: function() {
-			$('.l-nav .indicator').css("left","50%")
-		},
-		offset: 80
-	})
+    var waypoint = new Waypoint({
+        element: document.getElementById('what-we-do'),
+        handler: function() {
+            $('.l-nav .indicator').css("left", "50%")
+        },
+        offset: 80
+    });
 
-	var waypoint = new Waypoint({
-		element: document.getElementById('contact-us'),
-		handler: function() {
-			$('.l-nav .indicator').css("left","75%")
-		},
-		offset: 100
-	})
+    var waypoint = new Waypoint({
+        element: document.getElementById('contact-us'),
+        handler: function() {
+            $('.l-nav .indicator').css("left", "75%")
+        },
+        offset: 100
+    });
 
-	$(".nav-link").click(function(e) {
-		var scroll = "#" + $(this).attr("data-href");
-		$.scrollTo(scroll, {
-			duration: 600,
-			offset: -80
-		});
-	})
+    $(".nav-link").click(function(e) {
+        var scroll = "#" + $(this).attr("data-href");
+        $.scrollTo(scroll,
+            {
+                duration: 600,
+                offset: -80
+            });
+    });
 
-	$("a.get-in-touch").click(function(e) {
-		e.preventDefault();
-		$(".mvp-sprint-application").dreyanim({
-			animationType: "fallInAlternate",
-			animationTime: 350
-		})
-		$("body").css("overflow","hidden");
-	})
+    $("a.get-in-touch").click(function(e) {
+        e.preventDefault();
+        $.scrollTo("#contact-us", {
+            duration: 600,
+            offset: -80
+        })
+    });
 
-	$(".mvp-sprint-application .close-btn").click(function(e){
-		$(".mvp-sprint-application").dreyanim({
-			animationType: "fallOut",
-			animationTime: 400
-		})
-		$("body").css("overflow","auto");
-	})
+    $("button.mvp-sprint-button").click(function (e) {
+        e.preventDefault();
+        $(".mvp-sprint-application").dreyanim({
+            animationType: "fallInAlternate",
+            animationTime: 350
+        })
+        $("body").css("overflow", "hidden");
+    });
 
-	$(".form-message-wrapper .form-message-close").click(function(e) {
-		$(this).parent().addClass("hidden");
-	})
+    $(".mvp-sprint-application .close-btn").click(function(e) {
+        $(".mvp-sprint-application").dreyanim({
+            animationType: "fallOut",
+            animationTime: 400
+        })
+        $("body").css("overflow", "auto");
+    });
+
+    $(".form-message-wrapper .form-message-close").click(function(e) {
+        $(this).parent().addClass("hidden");
+    });
 })
